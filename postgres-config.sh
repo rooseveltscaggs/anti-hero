650f7092402907fa059a6d98624494ae7eb7b1f8
rooseveltscaggs <rscaggs@princeton.edu>
2023-11-03 20:40:20 -0400

    Added user variable

--- postgres-config.sh
+++ postgres-config.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+USER=$(whoami)
 sudo apt update
 sudo apt -y install postgresql postgresql-contrib
 sudo systemctl start postgresql.service
 cd /var/lib/postgresql
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'newpassword';"
 sudo ufw allow 5432/tcp
-sudo cp -rf /home/rscaggs/anti-hero/postgresql.conf /etc/postgresql/14/main/postgresql.conf
-sudo cp -rf /home/rscaggs/anti-hero/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf
+sudo cp -rf /home/$(USER)/anti-hero/postgresql.conf /etc/postgresql/14/main/postgresql.conf
+sudo cp -rf /home/$(USER)/anti-hero/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf
 sudo service postgresql restart
