170bbe1cab3f7a25686d9bdfa63a09b8aad3c332
rooseveltscaggs <rscaggs@princeton.edu>
2023-11-09 10:34:04 -0500

    Updated home to users

--- postgres-config.sh
+++ postgres-config.sh
@@ -8,8 +8,8 @@ cd /var/lib/postgresql
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'newpassword';"
 sudo ufw allow 5432/tcp
 sudo ufw allow 80/tcp
-sudo cp -rf /home/"$USER"/anti-hero/config/postgresql.conf /etc/postgresql/14/main/postgresql.conf
-sudo cp -rf /home/"$USER"/anti-hero/config/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf
+sudo cp -rf /users/"$USER"/anti-hero/config/postgresql.conf /etc/postgresql/14/main/postgresql.conf
+sudo cp -rf /users/"$USER"/anti-hero/config/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf
 sudo service postgresql restart
 
 echo "-- Server Types --"
@@ -20,13 +20,13 @@ echo -n "Enter Server Type to Install: "
 read x
 
 if [ $x == 1 ]; then
-  sudo cp -rf /home/"$USER"/anti-hero/antihero-orchestrator.service /etc/systemd/system/antihero.service
+  sudo cp -rf /users/"$USER"/anti-hero/antihero-orchestrator.service /etc/systemd/system/antihero.service
   sudo systemctl daemon-reload
   sudo systemctl start antihero-orchestrator
   sudo systemctl enable antihero-orchestrator
 elif [ $x == 2 ]; then
-  sudo cp -rf /home/"$USER"/anti-hero/antihero-server.service /etc/systemd/system/antihero.service
-  sudo cp -rf /home/"$USER"/anti-hero/antihero-serverbg.service /etc/systemd/system/antiherobg.service
+  sudo cp -rf /users/"$USER"/anti-hero/antihero-server.service /etc/systemd/system/antihero.service
+  sudo cp -rf /users/"$USER"/anti-hero/antihero-serverbg.service /etc/systemd/system/antiherobg.service
   sudo systemctl daemon-reload
   sudo systemctl start antihero-server
   sudo systemctl enable antihero-server
