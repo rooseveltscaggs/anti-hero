94f576526a63562a9f6fd3deaf8411865a2cd8d4
rooseveltscaggs <rscaggs@princeton.edu>
2024-01-18 03:02:33 -0500

    Added db close to retrieve_registry

--- orchestrator/main.py
+++ orchestrator/main.py
@@ -74,17 +74,18 @@ def retrieve_registry(key, default=None):
     registry_entry = db_session.query(RegistryEntry).filter(RegistryEntry.registry_name == key).first()
     if registry_entry:
         if registry_entry.int_value is not None:
-            return registry_entry.int_value
+            default = registry_entry.int_value
         
         if registry_entry.string_value is not None:
-            return registry_entry.string_value
+            default = registry_entry.string_value
         
         if registry_entry.bool_value is not None:
-            return registry_entry.bool_value
+            default = registry_entry.bool_value
         
         if registry_entry.datetime_value is not None:
-            return registry_entry.datetime_value
-        
+            default = registry_entry.datetime_value
+    
+    db_session.close() 
     return default
 
 
--- server/main.py
+++ server/main.py
@@ -69,18 +69,20 @@ def retrieve_registry(key, default=None):
     registry_entry = db_session.query(RegistryEntry).filter(RegistryEntry.registry_name == key).first()
     if registry_entry:
         if registry_entry.int_value is not None:
-            return registry_entry.int_value
+            default = registry_entry.int_value
         
         if registry_entry.string_value is not None:
-            return registry_entry.string_value
+            default = registry_entry.string_value
         
         if registry_entry.bool_value is not None:
-            return registry_entry.bool_value
+            default = registry_entry.bool_value
         
         if registry_entry.datetime_value is not None:
-            return registry_entry.datetime_value
-        
+            default = registry_entry.datetime_value
+    
+    db_session.close() 
     return default
+
 
 def update_server_map():
     orc_ip = retrieve_registry("Orchestrator_IP")
